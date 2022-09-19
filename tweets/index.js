9e00654f83ebed75bc70eaf328dd61e4e7d32db1
Nick Dalby <nckdlby@gmail.com>
2022-09-19 17:36:46 +0200

    edit

--- tweets/index.js
+++ tweets/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch')
 // amount can be between 5 and 100
 
 const getTweets = async (userId, amount) => {
-  const url = `https://api.twitter.com/2/users/${userId}/tweets?max_results=${amount}`
+  const url = `https://api.twitter.com/2/users/${userId}/tweets?max_results=${amount}?exclude=retweets?exclude=replies`
 
   const response = await fetch(url, {
     headers: { authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
